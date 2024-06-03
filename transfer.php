64098d0559bc841e69709c06598787f4a013e376
ACMAX <86621521+axolotl-freckles@users.noreply.github.com>
2024-06-03 14:34:23 -0600

    Added mock transfer api

--- transfer.php
+++ transfer.php
@@ -1,6 +1,25 @@
 <?php
+$MAX_AMOUNT = 1000.00;
 session_start();
-$user_val_str   = $_GET["val_str"];
+$user_val_str   = $_POST["val_str"];
 $server_val_str = $_SESSION["val_str"];
 
+if (empty($_POST["sender_account"])) {
+	echo("ERROR: No sender provided!");
+	exit(400);
+}
+if (empty($_POST["receiver_account"])) {
+	echo("ERROR: No receiver provided!");
+	exit(400);
+}
+if (empty($_POST["concept"])) {
+	echo("ERROR: No concept provided!");
+	exit(400);
+}
+if ($_POST["amount"] > $MAX_AMOUNT) {
+	echo("ERROR: amount surpases maximum tranference limit");
+	exit(400);
+}
+
+exit(200)
 ?>
--- validate_user.php
+++ validate_user.php
@@ -1,7 +1,12 @@
 <?php
 $username  = $_GET["username"];
-$pass_hash = $_GET["hash"];
+$pass_hash = password_hash($_GET["password"], PASSWORD_BCRYPT);
 $remote_addr = $_SERVER["REMOTE_ADDR"];
+
+if (empty($username)) {
+	echo("ERROR: no username provided!");
+	exit(400)
+}
 
 session_start();
 $_SESSION["val_str"] = $username."@".$remote_addr;
